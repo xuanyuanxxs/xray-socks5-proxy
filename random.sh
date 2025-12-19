6785a1a75d96c135e1fb0b6e09f75f86a48a7a2f
xuanyuanxxs <76103583+xuanyuanxxs@users.noreply.github.com>
2025-12-19 18:55:21 +0800

    Update random.sh

--- random.sh
+++ random.sh
@@ -133,7 +133,7 @@ echo "--- 3. 正在立即运行脚本进行初始化测试 (请注意网络中�
 echo "--- 运行 /etc/name.sh ---"
 #/etc/name.sh
 echo "--- 运行 /etc/mac.sh ---"
-/etc/mac.sh
+#/etc/mac.sh
 echo "--- 运行 /etc/ip.sh ---"
 #/etc/ip.sh
 
